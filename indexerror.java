fe6331471e9efc3fda41786d0fd83cf748f3ce05
hyejun <127915444+nojungsin@users.noreply.github.com>
2023-06-11 12:10:12 +0900

    Update indexerror.java

--- indexerror.java
+++ indexerror.java
@@ -1,2 +1,9 @@
 int[] numbers = {1, 2, 3};
 int value = numbers[3]; // ArrayIndexOutOfBoundsException: 유효하지 않은 인덱스에 접근
+
+try {
+                        sh "exit 1"
+                        currentBuild.result = 'SUCCESS'
+                        }catch (Exception err) {
+                        currentBuild.result = 'FAILURE'
+                        }
