9570e167b83378c9a26887bf8b9750bedf91da09
hyejun <127915444+nojungsin@users.noreply.github.com>
2023-06-14 06:23:14 +0900

    Update and rename class.java to Myclass.java

---
+++ Myclass.java
@@ -0,0 +1,7 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+        int value = numbers[3];
+        System.out.println(value);
+    }
+}
--- class.java
+++
@@ -1,2 +0,0 @@
-int[] numbers = {1, 2, 3};
-int value = numbers[3];
