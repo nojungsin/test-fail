e7e8ee60f314a87643965845ebfbe821e782aa4e
hyejun <127915444+nojungsin@users.noreply.github.com>
2023-06-14 06:44:49 +0900

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,7 @@
 public class MyClass {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3};
-        int value = numbers[3];
+        int value = numbers[2];
         System.out.println(value);
         sdklfkdnlkw
     }
